7f4704dcefbf4e958d4eae24072d4e81c61060d1
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:33:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class SubsystemA { public void A() => Console.WriteLine("A"); }
+class SubsystemB { public void B() => Console.WriteLine("B"); }
+
+class Facade
+{
+    SubsystemA a = new(); SubsystemB b = new();
+    public void Operation() { a.A(); b.B(); }
+}
+
+class Program { static void Main() => new Facade().Operation(); }
+
